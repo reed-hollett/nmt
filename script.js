f05a74e8952a00a606fc50510551dac155302c3a
reed-hollett <149107702+reed-hollett@users.noreply.github.com>
2023-10-27 12:48:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ container.addEventListener('click', (event) => {
         const newTortilla = document.createElement('div');
         newTortilla.classList.add('tortilla');
         newTortilla.style.left = `${Math.random() * 90 + 5}vw`;
+
+        // Randomly rotate the tortilla
+        newTortilla.style.transform = `translate(-50%, -50%) rotate(${Math.random() * 360}deg)`;
+        
         container.appendChild(newTortilla);
 
         setTimeout(() => {
-            newTortilla.style.transform = 'translateY(100vh)';
+            newTortilla.style.transform = `translateY(100vh) rotate(${Math.random() * 360}deg)`;
             newTortilla.style.opacity = '0';
         }, 100);
 
